26eaaafae3b2f00602bb5ae78f7d9d12f9f43828
Nelson Miranda <nmirandaghn@gmail.com>
2016-03-13 23:29:20 -0600

    Initiated the cart

---
+++ app/views/line_items/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @line_item, :id, :product_id, :integer, :cart_id, :integer, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228095610) do
+ActiveRecord::Schema.define(version: 20160314030952) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
